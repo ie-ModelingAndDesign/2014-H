2176d133217816b2e420305798df2e8103b5838b
小渡広和 <e135754@kowatarihirokazu-no-MacBook-Pro.local>
2014-12-03 21:20:54 +0900

    make project add function jump

---
+++ modelling/GameScene.swift
@@ -0,0 +1,65 @@
+//
+//  GameScene.swift
+//  modelling
+//
+//  Created by 小渡広和 on 2014/11/26.
+//  Copyright (c) 2014年 odo. All rights reserved.
+//
+
+import SpriteKit
+
+
+var moving: Int = 180
+var jp: Int = 0
+var time:Int = moving
+
+class GameScene: SKScene {
+    var player : SKSpriteNode!
+    var afterpos: CGPoint!
+    override func didMoveToView(view: SKView) {
+
+        //プレイヤーとなるキャラクター画像
+        self.player = SKSpriteNode(imageNamed:"bou")
+        self.addChild(player)
+        self.player.position = CGPointMake(300,300)
+        afterpos = CGPointMake(300,600)
+
+    }
+
+    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+        for touch: AnyObject in touches {
+            
+            // 移動時間を3秒に指定しています。
+            if(jp == 0){
+                if(time >= moving){
+                    jump()
+                    jp = 1
+                    time = 0
+                }
+            }
+        }
+    }
+
+    override func update(currentTime: CFTimeInterval) {
+        time += 1
+        if(jp == 1){
+            if(time >= moving){
+                down()
+                jp = 0
+                time = 0
+            }
+        }
+    }
+
+    func jump(){
+        afterpos = CGPointMake(300,600)
+        let travelTime = SKAction.moveTo(afterpos, duration: 3)
+        self.player.runAction(travelTime)
+    }
+
+    func down(){
+        afterpos = CGPointMake(300,300)
+        let travelTime2 = SKAction.moveTo(afterpos, duration: 3)
+        self.player.runAction(travelTime2)
+    }
+}
